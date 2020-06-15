26c40856ed07f1418ed4fddd0f8c6f2f48b115a3
Divia17 <diviadharmaraj@gmail.com>
2020-06-15 08:59:29 +0530

    header scroll

--- docs/function.js
+++ docs/function.js
@@ -17,3 +17,22 @@ function sendEmail() {
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("msg").value;
 }
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".firstSession");
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            header.classList.add("nav-scrolled");
+        } else {
+            header.classList.remove("nav-scrolled");
+        }
+    })
+}, sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
