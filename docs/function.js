7298d23ed2314578a916a3befed27558f55bab71
Divia17 <diviadharmaraj@gmail.com>
2020-06-27 04:18:01 +0530

    sec4 completion

--- docs/function.js
+++ docs/function.js
@@ -1,6 +1,20 @@
 // Defined Functions
 
-
+// $(".carousel").on("touchstart", function(event){
+//     var xClick = event.originalEvent.touches[0].pageX;
+// $(this).one("touchmove", function(event){
+//     var xMove = event.originalEvent.touches[0].pageX;
+//     if( Math.floor(xClick - xMove) > 5 ){
+//         $(this).carousel('next');
+//     }
+//     else if( Math.floor(xClick - xMove) < -5 ){
+//         $(this).carousel('prev');
+//     }
+// });
+// $(".carousel").on("touchend", function(){
+//         $(this).off("touchmove");
+// });
+// });
 
 var ModuletimeDelay = 7000;
 function loadMore() {
